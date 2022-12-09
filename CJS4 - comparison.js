c5f361a9fabde219c228f0c8bd5f581b15d617d9
Archiel73 <unikat10@hotmail.com>
2022-12-09 19:00:06 +0100

    sgd

--- CJS4 - comparison.js
+++ CJS4 - comparison.js
@@ -1,3 +1,25 @@
-.gj (){
+gj (){
     console.log ("uf..");
+}
+
+const club = {
+    type:"Fudbalski";
+    namu:"Chelsea"
+}
+console.log (club.type);
+console.log (club.namu);
+
+let select=document.getElementById("choicez")
+
+//profa pregleda testove, unosi broj poena, te program izbacuje ocjenu, naspram unesenih poena.
+"A" -> 90+
+"B" -> 80+
+"C" -> 70+
+"D" -> 60+
+"F" -> <=60
+
+function markz(){
+    console.log("poziva se na...");
+    let profsInputHtmlElemenet = documene
+    let=unos 
 }
